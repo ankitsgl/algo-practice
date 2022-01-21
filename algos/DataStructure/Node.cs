52122661ec39769dc290033e1dfe74c20a01bc6d
ankitsgl <ankit.singhal@gmail.com>
2022-01-20 21:09:40 -0600

    Node

---
+++ algos/DataStructure/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algos.DataStructure
+{
+    public class Node
+    {
+        public int Data { get; set; }
+
+        public IList<Node> Childs { get; set; }
+    }
+}
