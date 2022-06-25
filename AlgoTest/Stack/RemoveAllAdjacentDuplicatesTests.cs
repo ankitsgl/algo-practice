7398b9b4d119e57ea85767f86f8e2084555b1705
ankitsgl <ankit.singhal@gmail.com>
2022-06-25 17:39:20 -0500

    Remove All Adjacent Duplicates

---
+++ AlgoTest/Stack/RemoveAllAdjacentDuplicatesTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using algos.Stack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algos.Stack.Tests
+{
+    [TestClass()]
+    public class RemoveAllAdjacentDuplicatesTests
+    {
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            var sol = new RemoveAllAdjacentDuplicates();
+            var res = sol.Remove("deeedbbcccbdaa", 3);
+            Assert.AreEqual("aa", res);
+        }
+    }
+}
---
+++ algos/Stack/RemoveAllAdjacentDuplicates.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algos.Stack
+{
+    public class RemoveAllAdjacentDuplicates
+    {
+        private class Item
+        {
+            public char Char { get; set; }
+            public int Count{ get; set; }
+
+            public Item(char chr, int count = 1)
+            {
+                Char = chr;
+                Count = 1;
+
+            }
+        }
+        public string Remove(string data, int count)
+        {
+            var stack = new Stack<Item>();
+            foreach (var c in data)
+            {
+                if (stack.Count > 0 && stack.Peek().Char == c)
+                {
+                    var item = stack.Pop();
+                    item.Count = item.Count + 1;
+
+                    if (item.Count == count)
+                    {
+                        continue;
+                    }
+                    else
+                        stack.Push(item);
+                }
+                else
+                    stack.Push(new Item(c));
+            }
+            var sb = new StringBuilder();
+            while (stack.Count > 0)
+            {
+                var item = stack.Pop();                
+                    sb.Insert(0, new string(item.Char, item.Count));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
