118e9c9332544e66cf88c19ff0ada48d889a845e
ankitsgl <ankit.singhal@gmail.com>
2022-06-26 13:26:38 -0500

    Word Search 2

--- AlgoTest/Backtracking/WordSearchTests.cs
+++ AlgoTest/Backtracking/WordSearchTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using algos.Helpers;
 
 namespace algos.Backtracking.Tests
 {
@@ -15,13 +16,31 @@ namespace algos.Backtracking.Tests
         [TestMethod()]
         public void ExistTest()
         {
-            var board = new char[][] { 
+            var board = new char[][] {
                 new []{ 'A', 'B', 'C', 'E' },
                 new []{ 'S', 'F', 'C', 'S' },
                 new []{ 'A', 'D', 'E', 'E' },
             };
             var result = sol.Exist(board, "ABCCED");
             Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void FindWordsTest()
+        {
+            var board = new char[][] {
+                new [] {'o','a','a','n'},
+                new [] {'e','t','a','e'},
+                new [] {'i','h','k','r'},
+                new [] {'i','f','l','v'}
+            };
+            var result = sol.FindWords(board, new[] { "oath", "pea", "eat", "rain" });
+            Assert.AreEqual(2, result.Count);
+
+            ArrayHelper.PrintArray(result);
+
+            Assert.IsTrue(result.Contains("eat"));
+            Assert.IsTrue(result.Contains("oath"));
         }
     }
 }
--- algos/Backtracking/WordSearch.cs
+++ algos/Backtracking/WordSearch.cs
@@ -63,5 +63,66 @@ namespace algos.Backtracking
 
             return result;
         }
+
+        public IList<string> FindWords(char[][] board, string[] words)
+        {
+            var result = new List<string>();
+
+            var possibleDirections = new int[][]
+            {
+                new[] {1,0},
+                new[] {0,1},
+                new[] { -1, 0},
+                new[] { 0, -1}
+            };
+
+
+            bool IsValidLocation(int row, int col)
+            {
+                return row >= 0 && row < board.Length
+                    && col >= 0 && col < board[0].Length;
+            }
+            void backtrack(int row, int col, string word, int index)
+            {
+                if (!IsValidLocation(row, col))
+                    return;
+
+                if (board[row][col] != word[index])
+                    return;
+
+                if (board[row][col] == '#')
+                    return;
+
+                if (index == word.Length - 1)
+                {
+                    result.Add(word);
+                    return;
+                }
+                board[row][col] = '#';
+
+
+                foreach (var nextLocation in possibleDirections)
+                {
+                    backtrack(row + nextLocation[0],
+                        col + nextLocation[1], word, index + 1);
+                }
+                board[row][col] = word[index];
+            }
+
+
+            foreach (var word in words)
+            {
+                for (int row = 0; row < board.Length; row++)
+                {
+                    for (int col = 0; col < board[row].Length; col++)
+                    {
+                        backtrack(row, col, word, 0);
+                    }
+
+                }
+            }
+
+            return result;
+        }
     }
 }
