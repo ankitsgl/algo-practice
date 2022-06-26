a07e8723cc5e5394868d5cddb3f7506698da4b6f
ankitsgl <ankit.singhal@gmail.com>
2022-06-26 13:56:42 -0500

    SubSet

---
+++ AlgoTest/Backtracking/SubsetsTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using algos.Backtracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algos.Backtracking.Tests
+{
+    [TestClass()]
+    public class SubsetsTests
+    {
+        Subsets sol = new Subsets();
+        [TestMethod()]
+        public void SubsetsTest()
+        {
+            var result = sol.FindSubsets(new int[] { 1, 2, 3 });
+
+            Assert.AreEqual(8, result.Count);
+        }
+
+        [TestMethod()]
+        public void Subsets_opTest()
+        {
+            var result = sol.FindSubsets_Op(new int[] { 1, 2, 3 });
+
+            Assert.AreEqual(8, result.Count);
+        }
+    }
+}
---
+++ algos/Backtracking/Subsets.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algos.Backtracking
+{
+    public class Subsets
+    {
+        public List<List<int>> FindSubsets(int[] nums)
+        {
+            var result = new List<List<int>>();
+
+            result.Add(new List<int>());
+
+            foreach (var num in nums) 
+            {
+                var subSet = new List<List<int>>();
+                foreach (var curr in result)
+                {
+                    subSet.Add(new List<int>(curr) { num });
+                }
+                foreach(var curr in subSet)
+                    result.Add(curr);
+            }
+            return result;
+        }
+
+        public List<List<int>> FindSubsets_Op(int[] nums)
+        {
+            var result = new List<List<int>>();
+            var k = 0;
+            var n = nums.Length;
+
+            void backtrack(int first, List<int> curr)
+            {
+                // if the combination is done
+                if (curr.Count == k)
+                {
+                    result.Add(new List<int>(curr));
+                    return;
+                }
+                for (int i = first; i < n; ++i)
+                {
+                    // add i into the current combination
+                    curr.Add(nums[i]);
+                    // use next integers to complete the combination
+                    backtrack(i + 1, curr);
+                    // backtrack
+                    curr.RemoveAt(curr.Count - 1);
+                }
+            }
+
+
+            for (k = 0; k < n+1; k++)
+            {
+                backtrack(0, new List<int>());
+            }
+
+            return result;
+        }
+
+    }
+}
