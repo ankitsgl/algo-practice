33725cf772f7c87666437e3f0e7a7631a6469b97
ankitsgl <ankit.singhal@gmail.com>
2022-07-11 22:53:24 -0500

    Phone Keypath Backtrack Combination and Word Search problems

---
+++ AlgoTest/Backtracking/PhoneNumberLetterCombinationsTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using algos.Backtracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using algos.Helpers;
+
+namespace algos.Backtracking.Tests
+{
+    [TestClass()]
+    public class PhoneNumberLetterCombinationsTests
+    {
+        [TestMethod()]
+        public void LetterCombinationsTest()
+        {
+            var sol = new PhoneNumberLetterCombinations();
+            var result = sol.LetterCombinations("23");
+            var expected = new[] { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" };
+
+            Assert.AreEqual(expected.Length, result.Count);
+
+            ArrayHelper.PrintArray(expected, "Expected: ");
+            ArrayHelper.PrintArray(result, "Result: ");
+        }
+
+        [TestMethod()]
+        public void LetterCombinationsTest2()
+        {
+            var sol = new PhoneNumberLetterCombinations();
+            var result = sol.LetterCombinations("234");
+            var expected = new[] { "adg", "adh", "adi", "aeg", "aeh", "aei", "afg", "afh", "afi", "bdg", "bdh", "bdi", "beg", "beh", "bei", "bfg", "bfh", "bfi", "cdg", "cdh", "cdi", "ceg", "ceh", "cei", "cfg", "cfh", "cfi" };
+
+            ArrayHelper.PrintArray(expected, "Expected: ");
+            ArrayHelper.PrintArray(result, "Result: ");
+
+            Assert.AreEqual(expected.Length, result.Count);
+        }
+
+        [TestMethod()]
+        public void LetterCombinationsWordsTest()
+        {
+            var sol = new PhoneNumberLetterCombinations();
+            var result = sol.LetterCombinationsWords("228");
+            var expected = new[] { "bat", "cat"};
+
+            Assert.AreEqual(expected.Length, result.Count);
+
+            ArrayHelper.PrintArray(expected, "Expected: ");
+            ArrayHelper.PrintArray(result, "Result: ");
+        }
+    }
+}
---
+++ algos/Backtracking/PhoneNumberLetterCombinations.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algos.Backtracking;
+
+public class PhoneNumberLetterCombinations
+{
+
+    public IList<string> LetterCombinations(string digits)
+    {
+        var letterMap = new Dictionary<char, string>()
+            {
+                { '2', "abc" }, {'3', "def" }, {'4', "ghi" }, {'5', "jkl" },
+                { '6', "mno" }, {'7', "pqrs" }, {'8', "tuv" }, {'9', "wxyz" }
+            };
+
+        var combinations = new List<string>();
+        void backtrack(int index, StringBuilder path)
+        {
+            if (path.Length == digits.Length)
+            {
+                combinations.Add(path.ToString());
+                return;
+            }
+
+            foreach (char chr in letterMap[digits[index]])
+            {
+                path.Append(chr);
+                backtrack(index + 1, path);
+                path.Remove(path.Length - 1, 1);
+            }
+        }
+
+        backtrack(0, new StringBuilder());
+
+        return combinations;
+    }
+
+    public IList<string> LetterCombinationsWords(string digits)
+    {
+        var letterMap = new Dictionary<char, string>()
+            {
+                { '2', "abc" }, {'3', "def" }, {'4', "ghi" }, {'5', "jkl" },
+                { '6', "mno" }, {'7', "pqrs" }, {'8', "tuv" }, {'9', "wxyz" }
+            };
+
+        var dict = new HashSet<string>()
+        { "hat", "bat", "cat", "house", "was", "dol" };
+
+        var combinations = new List<string>();
+        void backtrack(int index, StringBuilder path)
+        {
+            if (path.Length == digits.Length)
+            {
+                var word = path.ToString();
+                if ( dict.Contains(word))
+                    combinations.Add(word);
+
+                return;
+            }
+
+            foreach (char chr in letterMap[digits[index]])
+            {
+                path.Append(chr);
+                backtrack(index + 1, path);
+                path.Remove(path.Length - 1, 1);
+            }
+        }
+
+        backtrack(0, new StringBuilder());
+
+        return combinations;
+    }
+}
