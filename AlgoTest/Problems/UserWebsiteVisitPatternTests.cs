fdfc044450906405b3285fe633dd1ad9e5ad945e
ankitsgl <ankit.singhal@gmail.com>
2022-07-26 23:35:24 -0500

    Page Count

---
+++ AlgoTest/Problems/UserWebsiteVisitPatternTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using algos.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using algos.Helpers;
+
+namespace algos.Problems.Tests;
+
+[TestClass()]
+public class UserWebsiteVisitPatternTests
+{
+    [TestMethod()]
+    public void MostVisitedPatternTest()
+    {
+        var username = new[] { "joe", "joe", "joe", "james", "james", "james", "james", "mary", "mary", "mary" };
+        var timestamp = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        var website = new[] { "home", "about", "career", "home", "cart", "maps", "home", "home", "about", "career" };
+        var sol = new UserWebsiteVisitPattern();
+        var result = sol.MostVisitedPattern(username, timestamp, website);
+        
+        ArrayHelper.PrintArray(result);
+
+        Assert.AreEqual(3, result.Count);
+
+        Assert.IsTrue(result.Contains("home"));
+        Assert.IsTrue(result.Contains("about"));
+        Assert.IsTrue(result.Contains("career"));
+    }
+    [TestMethod()]
+    public void MostVisitedPatternTest1()
+    {
+        var username = new[] { "dowg", "dowg", "dowg" };
+        var timestamp = new[] { 158931262, 562600350, 148438945 };
+        var website = new[] { "y", "loedo", "y" };
+        var sol = new UserWebsiteVisitPattern();
+        var result = sol.MostVisitedPattern(username, timestamp, website);
+
+        ArrayHelper.PrintArray(result);
+
+        Assert.AreEqual(3, result.Count);
+
+        Assert.AreEqual("y", result[0]);
+        Assert.AreEqual("y", result[1]);
+        Assert.AreEqual("loedo", result[2]);
+    }
+
+    [TestMethod()]
+    public void MostVisitedPatternTest2()
+    {
+        var username = new[] { "zkiikgv", "zkiikgv", "zkiikgv", "zkiikgv" };
+        var timestamp = new[] { 43, 71, 38, 79};
+        var website = new[] { "wnaaxbfhxp", "mryxsjc", "oz", "wlarkzzqht" };
+        var sol = new UserWebsiteVisitPattern();
+        var result = sol.MostVisitedPattern(username, timestamp, website);
+
+        ArrayHelper.PrintArray(result);
+
+        Assert.AreEqual(3, result.Count);
+      
+        Assert.AreEqual("oz", result[0]);
+        Assert.AreEqual("mryxsjc", result[1]);
+        Assert.AreEqual("wlarkzzqht", result[2]);
+    }
+}
---
+++ algos/Problems/UserWebsiteVisitPattern.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algos.Problems;
+
+public class UserWebsiteVisitPattern
+{
+    class Visit
+    {
+        public string User { get; set; }
+        public string Website { get; set; }
+        public int Timestamp { get; set; }
+    }
+
+    //https://leetcode.com/problems/analyze-user-website-visit-pattern/
+    public IList<string> MostVisitedPattern(string[] username, int[] timestamp, string[] website)
+    {
+
+        var map = new Dictionary<string, LinkedList<Visit>>();
+        var visits = new List<Visit>();
+        for (var i = 0; i < username.Length; i++)
+        {
+            visits.Add(new Visit()
+            {
+                Website = website[i],
+                User = username[i],
+                Timestamp = timestamp[i]
+            });
+        }
+
+        //visits.Sort((t1, t2) => { return t1.Timestamp.CompareTo(t2.Timestamp); });
+        var pageGroupMap = new Dictionary<string, int>();
+        foreach (var visit in visits)
+        {
+            if ( !map.ContainsKey(visit.User) )
+                map[visit.User] = new LinkedList<Visit>();
+
+            map[visit.User].AddLast(visit);
+
+            if (map[visit.User].Count == 3)
+            {
+                var group = map[visit.User].ToList();
+                group.Sort((t1, t2)=> t1.Timestamp.CompareTo(t2.Timestamp));
+                var key = string.Join('-', group.Select(s => s.Website).ToList());
+                if (!pageGroupMap.ContainsKey(key))
+                    pageGroupMap[key] = 0;
+                pageGroupMap[key]++;
+                map[visit.User].RemoveFirst();
+            }
+        }
+        var orderList = pageGroupMap.ToList();
+        orderList.Sort((t1, t2) => { 
+            if ( t1.Value == t2.Value)
+                return t1.Key.CompareTo(t2.Key); 
+            return t2.Value.CompareTo(t1.Value);
+        });
+
+        return orderList[0].Key.Split('-');
+    }
+
+}
